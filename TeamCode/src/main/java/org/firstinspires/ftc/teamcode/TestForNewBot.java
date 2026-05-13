03671f3cb9ee3e9f63fd8931fdc3f1d6a0290553
truman.eustace <true92410@gmail.com>
2026-05-13 14:33:24 -0400

    basically nothing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestForNewBot.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class TestForNewBot extends LinearOpMode {
+
+    public void runOpMode() throws InterruptedException {
+
+        waitForStart();
+
+
+    }
+}
